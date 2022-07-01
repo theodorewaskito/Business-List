9bdbc3d3fbdb88c855be8b3be9f426c925378ebd
theodorewaskito <theodorewaskito@gmail.com>
2022-07-01 20:18:52 +0700

    templating

--- business-project/src/App.vue
+++ business-project/src/App.vue
@@ -1,138 +1,51 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import Navbar from './components/Navbar.vue'
+  import { RouterLink, RouterView } from 'vue-router'
+  import Navbar from './components/Navbar.vue'
 
-export default {
-  name: "App",
-  data() {
-    return {
-      dataBusiness: [],
-      dataCategory: [],
-      dataBusinessbyCategory: []
-    }
-  },
+  // export default defineComponent ({
+  //   name: "App",
+  //   data() {
+  //     return {
+  //       dataBusiness: [],
+  //       dataCategory: [],
+  //       dataBusinessbyCategory: []
+  //     } 
+  //   },
 
-  methods: {
-    getBusinessData() {
-      fetch({
-        method: "post",
-        url: "http://sandbox.bizharedev.id:17001/business/parent/all",
-        headers: {
-          "Accept-Language" : "id"
-        },
-        data: {
-          "businessName": "string",
-          "size": 12,
-          "page": 1,
-          "listCategory": []
-        }
-      })
-    }
-  }
-}
+  //   methods: {
+  //     getBusinessData() {
+  //       fetch({
+  //         method: "post",
+  //         url: "http://sandbox.bizharedev.id:17001/business/parent/all",
+  //         headers: {
+  //           "Accept-Language" : "id"
+  //         },
+  //         data: {
+  //           "businessName": "string",
+  //           "size": 12,
+  //           "page": 1,
+  //           "listCategory": []
+  //         }
+  //       })
+  //     }
+  //   },
+
+  //   components: {
+  //     Navbar
+  //   }
+  // })
 </script>
 
 <template>
+
   <Navbar/>
 
-  <RouterView />
+  <div class="container my-3">
+    <RouterView />
+  </div>
 
 </template>
 
 <style>
-/* @import '@/assets/base.css';
 
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-} */
 </style>
--- business-project/src/views/HomeView.vue
+++ business-project/src/views/HomeView.vue
@@ -3,12 +3,55 @@
 </script>
 
 <template>
-  <div>
-    <form class="d-flex">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-    <button class="btn btn-outline-success">Kategori</button>
+  <div >
+    <div class="row">
+      <div class="col-10">
+        <div class="input-group mb-3">
+          <button class="btn btn-outline-secondary" type="button" id="button-addon2">
+            <i class="bi bi-search"></i>
+          </button>
+          <input type="text" class="form-control" placeholder="Cari nama bisnis" aria-label="Recipient's username" aria-describedby="button-addon2" style="background-color: #f1f5f9">
+
+        </div>
+      </div>
+      <div class="col-2">
+        <button 
+          class="btn btn-outline-primary" 
+          style="width: 100%"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          <i class="bi bi-folder"></i>
+          Kategori
+        </button>
+
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog  modal-dialog-scrollable">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Filter</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <p style="color: #c3c8cf">Kategori Bisnis</p>
+                <div class="form-check">
+                  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                  <label class="form-check-label" for="flexRadioDefault1">
+                    Default radio
+                  </label>
+                </div>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-primary" style="width: 100%">Terapkan</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    
+    
 
     <Card/>  
 
