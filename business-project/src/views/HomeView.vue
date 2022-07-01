a6c2439ee3492050420ddfd309d1bc676d2fc85f
theodorewaskito <theodorewaskito@gmail.com>
2022-07-01 17:27:08 +0700

    vue init

---
+++ business-project/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import Card from '../components/Card.vue'
+</script>
+
+<template>
+  <div>
+    <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+    <button class="btn btn-outline-success">Kategori</button>
+
+    <Card/>  
+
+    <!-- <p>Test</p> -->
+
+  </div>
+
+</template>
